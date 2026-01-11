a6c07cf1477bcbd77370073b96cb1d850c44c72d
WWMachine <130657577+WWMachine@users.noreply.github.com>
2026-01-11 12:21:47 +0100

    Update util.py

--- util.py
+++ util.py
@@ -581,7 +581,7 @@ def fill_features(hour,batch_data,prev_data):
 def add_first_price_features(fs,batch_data):
 
     
-    first_hour = (datetime.datetime.now(tz=pytz.UTC)).replace(hour=23, minute=0, second=0, microsecond=0) - datetime.timedelta(days=1) # 1 day is default
+    first_hour = (datetime.datetime.now(tz=pytz.UTC)).replace(hour=23, minute=0, second=0, microsecond=0) - datetime.timedelta(days=2) # 1 day is default
     
     price_group = fs.get_feature_group(name=f"price_swedene3", version=1)
     first_init_roll = price_group.filter(price_group.date >= first_hour -  datetime.timedelta(3)).read()
@@ -748,6 +748,7 @@ def upload_to_hops(project, today,pred_path,hind_path):
         dataset_api.mkdir("Resources/SE3")
     dataset_api.upload(pred_path, f"Resources/SE3/forecast_{str_today}", overwrite=True)
     dataset_api.upload(hind_path, f"Resources/SE3/hindcast_{str_today}", overwrite=True)
+
 
 
 
